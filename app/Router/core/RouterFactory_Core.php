d09f75f1b104b126fb5ceb15c8bf88f1b45db6b3
Wlezley Lishack <wlezley.lishack@gmail.com>
2023-11-11 03:16:55 +0100

    fragment RouterFactory to core/project

---
+++ app/Router/core/RouterFactory_Core.php
@@ -0,0 +1,22 @@
+<?php
+
+// declare(strict_types=1);
+
+namespace App;
+
+use Nette;
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
+
+
+abstract class CoreRouterFactory
+{
+    use Nette\StaticClass;
+
+    public static function createRouter(): RouteList
+    {
+        $router = new RouteList;
+        $router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
+        return $router;
+    }
+}
