8f44c28002068a149acce590a210628086d9f8a0
Wlezley Lishack <wlezley.lishack@gmail.com>
2023-11-11 02:49:13 +0100

    Basic config & router tuning

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -12,10 +12,11 @@ class Bootstrap
 	public static function boot(): Configurator
 	{
 		$configurator = new Configurator;
-		$appDir = dirname(__DIR__);
+		$appDir = __DIR__ . '/..'; // dirname(__DIR__);
 
 		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
 		$configurator->enableTracy($appDir . '/log');
+		// error_reporting(E_ERROR);
 
 		$configurator->setTimeZone('Europe/Prague');
 		$configurator->setTempDirectory($appDir . '/temp');
