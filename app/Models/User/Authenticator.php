27c2bf5beca60f65b2c440ac3d9ccdc495894340
Wlezley Lishack <wlezley.lishack@gmail.com>
2024-10-09 15:19:35 +0200

    Authenticator: User::TABLE_NAME, Exception mod, Session nástřel

--- app/Models/User/Authenticator.php
+++ app/Models/User/Authenticator.php
@@ -17,25 +17,35 @@ class Authenticator implements Nette\Security\Authenticator
     /** @var Nette\Database\Explorer */
     protected $db;
 
-    /** @var Passwords */
+    // /** @var Nette\Http\Request */
+    // private $request;
+
+    // /** @var Nette\Http\Session */
+    // private $session;
+
+    /** @var Nette\Security\Passwords */
     private $passwords;
 
     public function __construct(Explorer $db, Passwords $passwords)
     {
         $this->db = $db;
+        // $this->request = $request;
+        // $this->session = $session;
         $this->passwords = $passwords;
     }
 
     public function authenticate(string $username, string $password): SimpleIdentity
     {
-        $row = $this->db->table(User::TABLE)->where('name', $username)->fetch();
+        $row = $this->db->table(User::TABLE_NAME)->where('name', $username)->fetch();
 
         if (!($row && $this->passwords->verify($password, $row->password))) {
-            throw new AuthenticationException('Nesprávné přihlašovací údaje');
+            throw new AuthenticationException('Invalid credentials.', self::InvalidCredential);
         }
 
         $user = $row->toArray();
         unset($user['password']);
+
+        // TODO: Session handler ?
 
         return new SimpleIdentity($user['id'], $user['role'], $user);
     }
