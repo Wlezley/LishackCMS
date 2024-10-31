c30c56f4f910578c38afc71cc7548ccc6d86fae2
Wlezley Lishack <wlezley.lishack@gmail.com>
2024-10-31 11:23:10 +0100

    PHINX init

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -9,26 +9,26 @@ use Nette\Bootstrap\Configurator;
 
 class Bootstrap
 {
-	public static function boot(): Configurator
-	{
-		$configurator = new Configurator;
-		$appDir = __DIR__ . '/..';
+    public static function boot(): Configurator
+    {
+        $configurator = new Configurator;
+        $appDir = __DIR__ . '/..';
 
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy($appDir . '/log');
-		// error_reporting(E_ERROR);
+        //$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+        $configurator->enableTracy($appDir . '/log');
+        // error_reporting(E_ERROR);
 
-		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory($appDir . '/temp');
+        $configurator->setTimeZone('Europe/Prague');
+        $configurator->setTempDirectory($appDir . '/temp');
 
-		$configurator->createRobotLoader()
-			->addDirectory(__DIR__)
-			->register();
+        $configurator->createRobotLoader()
+            ->addDirectory(__DIR__)
+            ->register();
 
-		$configurator->addConfig($appDir . '/config/project/common.neon');
-		$configurator->addConfig($appDir . '/config/project/services.neon');
-		$configurator->addConfig($appDir . '/config/local.neon');
+        $configurator->addConfig($appDir . '/config/project/common.neon');
+        $configurator->addConfig($appDir . '/config/project/services.neon');
+        $configurator->addConfig($appDir . '/config/local.neon');
 
-		return $configurator;
-	}
+        return $configurator;
+    }
 }
