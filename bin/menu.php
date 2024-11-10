6748a0078f89506984dbde155f7c1f9598c3f8f2
Wlezley Lishack <wlezley.lishack@gmail.com>
2024-11-10 23:54:13 +0100

    MENU: Nested set model
    TODO:
    - Move method
    - Reorder method
    - Specific node loading
    - UrlGenerator
    - Get active menu item (by url or nyme_url?)

---
+++ bin/menu.php
@@ -0,0 +1,107 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$argcOffset = 2;
+$commandList = [
+    '--create' => 1,
+    '--delete' => 1,
+    // '--move' => 2,
+    '--list' => 0,
+];
+
+if (isset($argv[1]) && in_array($argv[1], array_keys($commandList))) {
+    $command = $argv[1];
+    $argcRequired = $commandList[$command] + $argcOffset;
+}
+
+$container = App\Bootstrap::boot()->createContainer();
+$manager = $container->getByType(App\Models\Menu::class);
+
+switch ($command) {
+    case '--create':
+        if ($argc < $argcRequired) {
+            echo 'Usage: menu.php --create <title> [<parent_id>]';
+            exit(1);
+        }
+
+        $title = $argv[2];
+        $parentId = $argv[3] ? (int)$argv[3] : NULL;
+
+        try {
+            $id = $manager->addMenuItem($title, $parentId);
+            echo "Menu '$title' was added [ID: $id].\n";
+        } catch (\Exception $e) {
+            echo "Error: " . $e->getMessage() . "\n";
+            exit(1);
+        }
+        break;
+
+    case '--delete':
+        if ($argc != $argcRequired) {
+            echo 'Usage: menu.php --delete <id>';
+            exit(1);
+        }
+
+        $id = (int)$argv[2];
+
+        try {
+            $manager->removeMenuItem($id);
+            echo "Menu item '$id' was removed (recursive).\n";
+        } catch (\Exception $e) {
+            echo "Error: " . $e->getMessage() . "\n";
+            exit(1);
+        }
+        break;
+
+    // case '--move':
+    //     if ($argc != $argcRequired) {
+    //         echo 'Usage: menu.php --move <id> <new_parent_id>';
+    //         exit(1);
+    //     }
+
+    //     $id = (int)$argv[2];
+    //     $newParentId = $argv[3] ? (int)$argv[3] : NULL;
+
+    //     try {
+    //         $manager->moveMenuItem($id, $newParentId);
+    //         $manager->reorderMenuTree();
+    //         echo "Menu item '$id' was moved to '$newParentId' (recursive).\n";
+    //     } catch (\Exception $e) {
+    //         echo "Error: " . $e->getMessage() . "\n";
+    //         exit(1);
+    //     }
+    //     break;
+
+    case '--list':
+        if ($argc != $argcRequired) {
+            echo 'Usage: user.php --list';
+            exit(1);
+        }
+
+        try {
+            $menuList = $manager->getMenuTree();
+            print_r($menuList);
+        } catch (\Exception $e) {
+            echo "Error: " . $e->getMessage() . "\n";
+            exit(1);
+        }
+        break;
+
+    default:
+        echo '
+=== LISHACK CMS :: MENU MANAGER ===
+
+Usage: menu.php <command> [...]
+
+Available comannds:
+    --create <title> [<parent_id>]      Create new menu item
+    --delete <id>                       Delete menu item
+    --list                              Show menu list
+
+';
+        exit(0);
+}
---
+++ db/migrations/20241110161937_menu_system.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class MenuSystem extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute("
+            CREATE TABLE `menu` (
+                `id` INT(11) NOT NULL AUTO_INCREMENT,
+                `parent_id` INT(11) NULL DEFAULT NULL,
+                `lft` INT(11) NULL DEFAULT NULL,
+                `rgt` INT(11) NULL DEFAULT NULL,
+                `depth` INT(11) NULL DEFAULT NULL,
+                `name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+                `name_url` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+                `title` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+                `description` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+                `body` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+                `hidden` ENUM('0','1') NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+                PRIMARY KEY (`id`) USING BTREE
+            )
+            COLLATE='utf8mb4_general_ci'
+            ENGINE=InnoDB;
+
+            INSERT INTO `menu` (`id`, `parent_id`, `lft`, `rgt`, `depth`, `name`, `name_url`, `title`, `description`, `body`, `hidden`) VALUES
+                (1, 0, 1, 2, 0, NULL, NULL, 'MAIN_MENU', NULL, NULL, '0');
+        ");
+    }
+
+    public function down()
+    {
+    }
+}
