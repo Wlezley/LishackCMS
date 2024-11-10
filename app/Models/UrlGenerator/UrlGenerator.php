62601368a025aac5e501cfdef731757cdb5fd869
Wlezley Lishack <wlezley.lishack@gmail.com>
2024-11-10 23:45:35 +0100

    UrlGenerator (dummy class, todo...)

---
+++ app/Models/UrlGenerator/UrlGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+class UrlGenerator extends BaseModel
+{
+    // TODO ...
+    public function getUrl(): string
+    {
+        return "";
+    }
+}
