4643c9a41c9555cdea242da711154e65e38a6e46
Wlezley Lishack <wlezley.lishack@gmail.com>
2024-11-12 17:32:34 +0100

    DB cms_config - basic configuration data

---
+++ db/migrations/20241112125517_basic_cms_config.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class BasicCmsConfig extends AbstractMigration
+{
+    public function up()
+    {
+        $this->execute("
+            INSERT INTO `cms_config` (`name`, `category`, `value`) VALUES
+                ('APP_NAME', 'SYS', 'Lishack CMS'),
+                ('SITE_TITLE', 'SYS', 'Lishack CMS'),
+                ('DEFAULT_LANG', 'SYS', 'CZ'),
+                ('SEO_INDEX', 'SEO', 'SEO_INDEX'),
+                ('SEO_DESCRIPTION', 'SEO', 'SEO_DESCRIPTION'),
+                ('SEO_CANONICAL', 'SEO', 'SEO_CANONICAL'),
+                ('SOCIAL_TITLE', 'SOCIAL', 'SOCIAL_TITLE'),
+                ('SOCIAL_DESCRIPTION', 'SOCIAL', 'SOCIAL_DESCRIPTION'),
+                ('SOCIAL_IMAGE', 'SOCIAL', 'SOCIAL_IMAGE'),
+                ('LANG', 'SYS', 'CZ');
+        ");
+    }
+
+    public function down()
+    {
+        $this->execute("TRUNCATE TABLE `cms_config`;");
+    }
+}
