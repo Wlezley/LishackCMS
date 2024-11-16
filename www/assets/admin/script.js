b0252b9e3e76a1b31fc741d3a1f4710a8860bd81
Wlezley Lishack <wlezley.lishack@gmail.com>
2024-11-16 18:52:10 +0100

    Admin: Sticky menu

--- www/assets/admin/script.js
+++ www/assets/admin/script.js
@@ -1,4 +1,8 @@
 const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
+    document.querySelector("#content").classList.toggle("collapsed");
+    document.querySelector("#content").classList.toggle("fullsize");
+    document.querySelector("#menu-switch").classList.toggle("collapsed");
+    document.querySelector("#menu-switch").classList.toggle("fullsize");
 });
