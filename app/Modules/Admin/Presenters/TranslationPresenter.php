5cccf69e4bc1049dd0f906316cc3e84f518ae702
Wlezley Lishack <wlezley.lishack@gmail.com>
2025-03-17 09:58:33 +0100

    Admin:TranslationPresenter: lang data

--- app/Modules/Admin/Presenters/TranslationPresenter.php
+++ app/Modules/Admin/Presenters/TranslationPresenter.php
@@ -9,6 +9,14 @@ class TranslationPresenter extends SecuredPresenter
     public function renderDefault(int $page = 1, ?string $lang = null, ?string $search = null): void
     {
         $lang = $lang ?? DEFAULT_LANG;
+
+        $langData = $this->translationManager->getLanguageData($lang);
+        if ($langData === null) {
+            $this->redirect('Translation:');
+        }
+
+        $this->template->title = 'Jazykový překlad (' . $langData['name'] . ')';
+
         $limit = 10;
         $offset = ($page - 1) * $limit;
 
@@ -22,7 +30,7 @@ class TranslationPresenter extends SecuredPresenter
         $totalItems = $this->translationManager->getCount($lang, $search);
         $this->setPagination($limit, $totalItems);
 
-        $this->template->title = "Jazykový překlad [$lang]";
+        $this->template->lang = ($lang == DEFAULT_LANG) ? '' : $lang;
         $this->template->totalItems = $totalItems;
         $this->template->search = $search;
     }
