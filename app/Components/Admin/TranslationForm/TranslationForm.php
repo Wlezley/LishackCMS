adc5bddae9c81a21ac6b9fe34bcbeb1f79799982
Wlezley Lishack <wlezley.lishack@gmail.com>
2025-03-18 03:20:58 +0100

    Admin: Translation Form

---
+++ app/Components/Admin/TranslationForm/TranslationForm.php
@@ -0,0 +1,144 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Components\Admin;
+
+use App\Components\BaseControl;
+use Nette\Application\UI\Form;
+
+class TranslationForm extends BaseControl
+{
+    public const OriginCreate = 'Create';
+    public const OriginEdit = 'Edit';
+
+    private string $origin;
+
+    /** @var array<string,array<string,mixed>> */
+    private array $languageList;
+
+    /** @var array<string,string> $queryParams */
+    private array $queryParams;
+
+    /** @var callable(string): void */
+    public $onSuccess;
+
+    /** @var callable(string): void */
+    public $onError;
+
+    public function __construct(
+        protected \Nette\Security\User $user
+    ) {
+        $this->param = [];
+    }
+
+    public function createComponentForm(): Form
+    {
+        $param = $this->param;
+        bdump($param, "FORM PARAM");
+
+        $form = new Form();
+
+        $form->setHtmlAttribute('autocomplete', 'off');
+
+        $form->addText('key', 'Klíč *')
+            ->setHtmlAttribute('autocomplete', 'off')
+            ->setValue($param['key'] ?? '')
+            ->setRequired();
+
+        foreach ($this->languageList as $key => $langData) {
+            // bdump($langData);
+            $required = $langData['default'] == 1;
+            $form->addTextArea("text_$key", "Text ($langData[name])" . ($required ? ' *' : ''), null, 1)
+                ->setHtmlAttribute('autocomplete', 'off')
+                ->setValue($param["text_$key"] ?? '')
+                ->setRequired($required);
+        }
+
+        $form->addSubmit('save');
+
+        $form->onSuccess[] = [$this, 'process' . $this->origin];
+
+        return $form;
+    }
+
+    /** @param \Nette\Utils\ArrayHash<mixed> $values */
+    public function processCreate(Form $form, \Nette\Utils\ArrayHash $values): void
+    {
+        bdump($values, "TRANSLATION FORM CREATE VALUES");
+
+        if (!empty($this->translationManager->getTextListByKey($values['key']))) {
+            $editLink = $this->buildEditUrl($values['key']);
+            bdump($editLink);
+
+            $editAnchor = '<a href="' . $editLink . '" target="_blank">' . $values['key'] . ' <sup><i class="fa fa-external-link"></i></sup></a>';
+            call_user_func($this->onError, "Překlad pro <strong>$values[key]</strong> již existuje. Přejmenujte klíč, nebo upravte stávající $editAnchor");
+        } else {
+            foreach ($this->languageList as $lang => $langData) {
+                if (isset($values["text_$lang"])) {
+                    if (empty($values["text_$lang"])) {
+                        continue;
+                    }
+
+                    $this->translationManager->add($values['key'], $lang, $values["text_$lang"]);
+                }
+            }
+
+            if (!empty($this->translationManager->getTextListByKey($values['key']))) {
+                call_user_func($this->onSuccess, "Překlad pro <strong>$values[key]</strong> byl vytvořen.");
+            } else {
+                call_user_func($this->onError, 'Překlad se nepodařilo vytvořit.');
+            }
+        }
+    }
+
+    /** @param \Nette\Utils\ArrayHash<mixed> $values */
+    public function processEdit(Form $form, \Nette\Utils\ArrayHash $values): void
+    {
+        bdump($values, "TRANSLATION FORM EDIT VALUES");
+
+        $key = $values['key'];
+        $textList = $this->translationManager->getTextListByKey($key);
+
+        foreach ($this->languageList as $lang => $langName) {
+            if (isset($textList[$lang])) {
+                $this->translationManager->save($key, $lang, $values["text_$lang"]); // TODO: Dlete if "$values["text_$lang"]" is empty (???)
+            } elseif (!empty($values["text_$lang"])) {
+                $this->translationManager->add($key, $lang, $values["text_$lang"]);
+            }
+        }
+
+        call_user_func($this->onSuccess, 'Překlad byl upraven');
+    }
+
+    public function render(int|string|null $key = null): void
+    {
+        $this->template->languageList = $this->languageList;
+        $this->template->setFile(__DIR__ . '/TranslationForm' . $this->origin . '.latte');
+        $this->template->render();
+    }
+
+    public function setOrigin(string $origin): void
+    {
+        $this->origin = $origin;
+    }
+
+    /** @param array<string,array<string,mixed>> $languageList */
+    public function setLanguageList(array $languageList): void
+    {
+        $this->languageList = $languageList;
+    }
+
+    /** @param array<string,string> $queryParams */
+    public function setQueryParams(array $queryParams): void
+    {
+        unset($queryParams['page']);
+        $this->queryParams = $queryParams;
+    }
+
+    private function buildEditUrl(string $key): string
+    {
+        $params = http_build_query(array_merge($this->queryParams, ['key' => $key]));
+        return '?' . $params;
+    }
+}
--- app/Modules/Admin/Presenters/TranslationPresenter.php
+++ app/Modules/Admin/Presenters/TranslationPresenter.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace App\Modules\Admin\Presenters;
 
+use App\Components\Admin\ITranslationFormFactory;
+
 class TranslationPresenter extends SecuredPresenter
 {
+    /** @var ITranslationFormFactory @inject */
+    public ITranslationFormFactory $translationForm;
+
     public function renderDefault(int $page = 1, ?string $lang = null, ?string $search = null): void
     {
         $lang = $lang ?? $this->translationManager->getLanguageService()->getDefaultLang(DEFAULT_LANG);
@@ -38,5 +43,49 @@ class TranslationPresenter extends SecuredPresenter
     public function renderCreate(): void
     {
         $this->template->title = 'Nový jazykový překlad';
+    }
+
+    public function renderEdit(string $key): void
+    {
+        $this->template->title = "Editace překladu '$key'";
+    }
+
+    // ##########################################
+    // ###             COMPONENTS             ###
+    // ##########################################
+
+    protected function createComponentTranslationForm(): \App\Components\Admin\TranslationForm
+    {
+        $form = $this->translationForm->create();
+        $key = $this->getParameter('key');
+
+        if ($key) {
+            $form->setOrigin($form::OriginEdit);
+
+            $param['key'] = $key;
+            foreach ($this->translationManager->getTextListByKey($key) as $key => $text) {
+                $param["text_$key"] = $text;
+            }
+
+            $form->setParam($param);
+        } else {
+            $form->setOrigin($form::OriginCreate);
+            $form->setQueryParams($this->getHttpRequest()->getQuery());
+            $form->setParam($this->getHttpRequest()->getPost('param'));
+        }
+
+        $form->setLanguageList($this->translationManager->getLanguageService()->getList(false));
+
+        $form->onSuccess = function(string $message): void {
+            $this->flashMessage($message, 'info');
+            $this->redirect('Translation:default');
+        };
+
+        $form->onError = function(string $message): void {
+            $this->flashMessage($message, 'danger');
+            // $this->redirect('this');
+        };
+
+        return $form;
     }
 }
